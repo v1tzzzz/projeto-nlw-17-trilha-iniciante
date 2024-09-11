260f5d77a5c8d2a94886aa32b9b30d76b5630aa1
Alysson Vitor <aly.vtzcode@gmail.com>
2024-09-10 23:24:22 -0300

    fundamentos 003

--- index.js
+++ index.js
@@ -1,15 +1,20 @@
-// Arreys e Obejtos 
-
+// Cria uma meta 
 let meta = {
-    value: 'ler um livro todo mes',
-    checked: false
+    value: 'ler um livro todo dia',
+    checked: true
 }
 
-console.log(meta.checked);
+// Cria uma lista de METAS e podemos manipular o obejeto meta para usarmos depois
+let metas = [
+    meta,
+    {
+        value: 'caminhar todo dia',
+        checked: false
+    }
+]
 
+// console.log(metas[0].value ou checked) pega o primeiro objeto. 
+// Se colocarmos no [] o valor 1 ela o os parametros dentro do arrey 
+// Se atente que usei o metaS pois se usassemos o meta com [] daria erro 
 
-// Function 
-function criarMeta() {}
-
-// Arrow function 
-const criarMeta = () => {}
+console.log(metas[1].value);
